30e202d7c7ad3775be8e515412c11057f67f6e35
cameronjacoby <cameron.jacoby@gmail.com>
2015-12-02 13:54:22 -0800

    add solution

---
+++ config/routes.rb
@@ -0,0 +1,4 @@
+Rails.application.routes.draw do
+  root to: "creatures#index"
+  resources :creatures
+end
---
+++ db/seeds.rb
@@ -0,0 +1,2 @@
+Creature.create({name: "Luke", description: "Jedi"})
+Creature.create({name: "Darth Vader", description: "Father of Luke"})
