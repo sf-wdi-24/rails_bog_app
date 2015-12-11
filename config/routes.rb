4cfad1ef7bd676b73676c5e0ae7adf1ad453b68f
vframbach <vframbach@gmail.com>
2015-12-11 15:43:43 -0800

    add show

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   root to: "creatures#index"
 
   # use the resources method to have Rails make an index route for creatures
-  resources :creatures, only: [:index]
+  resources :creatures, only: [:index, :new, :create, :show]
 
   # resources :creatures, only: [:index] is equivalent to:
   # get "/creatures", to: "creatures#index"
+  # get "/creatures/new", to: "creatures#new"
+  # post "/creatures", to: "creatures#create"
+  # post "/creatures/:id", to: "creatures#show"
 end
