262cd3e218c884cb449b5a3a9fb57b851efb593a
mrockway <mrockway@gmail.com>
2015-12-11 15:20:24 -0800

    commit

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -1,6 +1,34 @@
 class CreaturesController < ApplicationController
 
+	# show homepage
 	def index
 		@creatures = Creature.all
 	end
+
+	# show new creature form
+	def new
+		@creature = Creature.new
+	end
+
+	# create new creature
+	def create
+		creature_params = params.require(:creature).permit(:name, :description)
+
+		creature = Creature.new(creature_params)
+
+		if creature.save
+			redirect_to creature_path(creature)
+		end
+	end
+
+	def show
+		creature_id = params[:id]
+		@creature = Creature.find_by_id(creature_id)
+		render :show
+	end
+
+	def edit
+		creature_id = params[:id]
+		@creature = Creature.find_by_id(creature_id)
+		render :edit
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   
   root to: "creatures#index"
   
-  resources :creatures, only: [:index]
+  resources :creatures, only: [:index, :new, :create, :show, :edit]
   
 end
