7232291428bf9af3a77224c931d3e5c3f0ff8e28
vframbach <vframbach@gmail.com>
2015-12-11 15:04:38 -0800

    add create, display all creatures

---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+Creature.create({name: "Luke", description: "Jedi"})
+Creature.create({name: "Darth Vader", description: "Father of Luke"})
