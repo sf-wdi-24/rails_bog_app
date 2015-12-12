bdef6fd9634257319d7608c82102c00cea92595f
abeytaadam <measalion@gmail.com>
2015-12-11 16:17:53 -0800

    BOG

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -41,4 +41,11 @@ class CreaturesController < ApplicationController
 		redirect_to creature_path(creature)
 	end
 
+	def destroy
+		creature_id = params[:id]
+		creature = Creature.find_by_id(creature_id)
+		creature.destroy
+		redirect_to creatures_path
+	end
+
 end
