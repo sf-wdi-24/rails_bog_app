7ada974311dd6274a0d818e1be68797dbb7a16f4
stevennoble78 <stevennoble78@hotmail.com>
2015-12-12 13:27:48 -0800

    add new, create, and show

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -3,4 +3,21 @@ class CreaturesController < ApplicationController
 		@creatures = Creature.all
 	end
 
+	def new
+		@creature = Creature.new
+	end
+
+	def create
+		creature_params = params.require(:creature).permit(:name, :description)
+		creature = Creature.new(creature_params)
+		if creature.save
+			redirect_to creature_path(creature)
+		end
+	end
+
+	def show
+		creature_id = params[:id]
+		@creature = Creature.find_by_id(creature_id)
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: "creatures#index"
 
-  resources :creatures, only: [:index]
+  resources :creatures, only: [:index, :new, :create, :show]
 end
