1c3d3a0b885ec34d49952f9103975bb0fa5553eb
stevennoble78 <stevennoble78@hotmail.com>
2015-12-12 15:40:41 -0800

    add update

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -20,4 +20,16 @@ class CreaturesController < ApplicationController
 		@creature = Creature.find_by_id(creature_id)
 	end
 
+	def edit
+		creature_id = params[:id]
+		@creature = Creature.find_by_id(creature_id)
+	end
+
+	def update
+		creature_id = params[:id]
+		creature = Creature.find_by_id(creature_id)
+		creature_params = params.require(:creature).permit(:name, :description)
+		creature.update_attributes(creature_params)
+		redirect_to creature_path(creature)
+	end
 end
