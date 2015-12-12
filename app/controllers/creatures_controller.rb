d59e6f4e458827269d129da094e80e5a7e8e51e1
mrockway <mrockway@gmail.com>
2015-12-11 16:35:41 -0800

    completed app w/ styling

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -31,4 +31,21 @@ class CreaturesController < ApplicationController
 		creature_id = params[:id]
 		@creature = Creature.find_by_id(creature_id)
 		render :edit
+	end
+
+	def update
+		creature_id = params[:id]
+		creature = Creature.find_by_id(creature_id)
+		creature_params = params.require(:creature).permit(:name, :description)
+		creature.update_attributes(creature_params)
+		redirect_to creature_path(creature)
+	end
+
+	def destroy
+		creature_id = params[:id]
+		creature = Creature.find_by_id(creature_id)
+		creature.destroy
+		redirect_to creatures_path
+	end
+
 end
