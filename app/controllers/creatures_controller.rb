070dc2f060270129e0f505426f206494135d0475
Cynthia Salinas <csalinas123@gmail.com>
2015-12-13 13:02:38 -0800

    modify index page

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -1,14 +1,12 @@
 class CreaturesController < ApplicationController
-  
-  # display all creatures 
+
+  # display all creatures
   def index
     # get all creatures from db and save to instance variable
     @creatures = Creature.all
 
-    # render the index view (it has access to instance variable) 
+    # render the index view (it has access to instance variable)
     render :index
-    end 
-
   end
 
   # show the new creature form
@@ -17,74 +15,89 @@ class CreaturesController < ApplicationController
     render :new
   end
 
+  # create a new creature in the database
+  def create
+    # whitelist params and save them to a variable
+    creature_params = params.require(:creature).permit(:name, :description)
+
+    # create a new creature from `creature_params`
+    creature = Creature.new(creature_params)
+
+    # if creature saves, redirect to route that displays
+    # ONLY the newly created creature
+    if creature.save
+      redirect_to creature_path(creature)
+      # redirect_to creature_path(creature) is equivalent to:
+      # redirect_to "/creatures/#{creature.id}"
+    end
+  end
+
   # display a specific creature
   def show
     # get the creature id from the url params
     creature_id = params[:id]
-    # use "creature_id" to find the creature in db and save to instance variable
+
+    # use `creature_id` to find the creature in the database
+    # and save it to an instance variable
     @creature = Creature.find_by_id(creature_id)
 
-    # render the show view(it has access to instance variable)
+    # render the show view (it has access to instance variable)
     render :show
   end
 
+  # show the edit creature form
   def edit
     # get the creature id from the url params
     creature_id = params[:id]
 
-    # use the 'creature_id' to find the creature in the db and save it to an instance variable 
+    # use `creature_id` to find the creature in the database
+    # and save it to an instance variable
     @creature = Creature.find_by_id(creature_id)
 
-    # render the edit view(it has access to an instance variable)
+    # render the edit view (it has access to instance variable)
     render :edit
   end
 
-  # show new creature in db
-  def create
-    # whitelist params and save to a variable
+  # update a creature in the database
+  def update
+    # get the creature id from the url params
+    creature_id = params[:id]
+
+    # use `creature_id` to find the creature in the database
+    # and save it to an instance variable
+    creature = Creature.find_by_id(creature_id)
+
+    # whitelist params and save them to a variable
     creature_params = params.require(:creature).permit(:name, :description)
 
-    # create new creature
-    creature = Creature.new(creature_params)
+    # update the creature
+    creature.update_attributes(creature_params)
 
-    # after saving,redirect to route that displays ONLY the new created creature
-    if creature.save
-      redirect_to creature_path(creature)
-    end 
-end
-    # update a creature in the db
-    def update
-      # get the creature id form the url params
-      creature_id = params [:id]
-
-      # use 'creature_id' to find the creature in the db and save it to an instance variable
-      creature = Creature.find_by_id(creature_id)
-
-      # whitelist params and save to a variable
-      creature_params = params.require(:creature).permit(:name, :description)
-
-      # update the creature
-      creature.update_attributes(creature_params)
-
-      # redirect to show page for the updated creature
-      redirect_to creature_path(creature)
-    end
-
-    # delete a creature from the db
-    def destroy 
-      # get the creature id from the url params
-      creature_id = params [:id]
-
-      # use 'creature-id' to find the creature in the db and save it to an instance variable
-      creature = Creature.find_by_id(creature_id)
-
-      # destroy the creature
-      creature.destroy
-
-      # redirect to creature index
-      redirect_to creatures_path
+    # redirect to show page for the updated creature
+    redirect_to creature_path(creature)
+    # redirect_to creature_path(creature) is equivalent to:
+    # redirect_to "/creatures/#{creature.id}"
   end
 
+  # delete a creature from the database
+  def destroy
+    # get the creature id from the url params
+    creature_id = params[:id]
+
+    # use `creature_id` to find the creature in the database
+    # and save it to an instance variable
+    creature = Creature.find_by_id(creature_id)
+
+    # destroy the creature
+    creature.destroy
+
+    # redirect to creatures index
+    redirect_to creatures_path
+    # redirect_to creatures_path is equivalent to:
+    # redirect_to "/creatures"
+  end
+
+end
 
 
 
