f568015b204f73e74d85e322da3002aed7e14c86
Jsdwyer26 <Jsdwyer26@gmail.com>
2015-12-12 22:25:36 -0800

    have new, create and show routes, methods, and views set

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -3,14 +3,23 @@ class CreaturesController < ApplicationController
     @creatures = Creature.all
     render :index
   end
-
   def new
+    @creature = Creature.new
+    render :new
   end
 
   def create
+    creature_params = params.require(:creature).permit(:name, :description)
+    creature = Creature.new(creature_params)
+    if creature.save
+      redirect_to creature_path(creature)
+    end
   end
 
   def show
+    creature_id = params[:id]
+    @creature = Creature.find_by_id(creature_id)
+    render :show
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
 
   root "creatures#index"
-  resources :creatures, only: [:index]
+  resources :creatures, only: [:index, :new, :create, :show]
+
+
+	 #get "/creatures", to: "creatures#index" -- (view:index, shows all creatures)
+	 #get "/creatures/new", to: "creatures#new" -- (view:new, shows form to create new creature)
+	 #post "/creatures", to: "creatures#create" -- (add new creature to db)
+	 #get "/creatures/:id", to: ":creatures#show" -- (view:show, show one creature)
+	 #get "/creatures/:id/edit", to: "creatures#edit" -- (view:edit, show form to edit one creature)
+	 #put "/creatures/:id", to: "creatures#update" -- (update one creature in db)
+	 #delete "/creatures/:id", to: "creatures#destroy" -- (destroy one creature in db)
 
 end
