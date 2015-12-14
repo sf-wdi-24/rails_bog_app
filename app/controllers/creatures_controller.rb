132be255063c3a8677c38522bee116e5d25d9c46
murcielago17 <murcielago17@hotmail.com>
2015-12-13 20:39:12 -0800

    Set up the creatures controller and index action

---
+++ app/controllers/creatures_controller.rb
@@ -0,0 +1,16 @@
+#
+# app/controllers/creatures_controller.rb
+#
+
+class CreaturesController < ApplicationController
+
+  # display all creatures
+  def index
+    # get all creatures from db and save to instance variable
+    @creatures = Creature.all
+
+    # render the index view (it has access to instance variable)
+    render :index
+  end
+
+end
