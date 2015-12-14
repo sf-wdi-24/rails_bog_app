ae87763e4dbb5ef2ab9fb26ad107943b50a5bfdd
murcielago17 <murcielago17@hotmail.com>
2015-12-13 22:03:12 -0800

    Define a route to create creatures in the database

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   root "creatures#index"
   # use the resources method to have Rails make an index route for creatures
-  resources :creatures, only: [:index, :new]
-  # resources :creatures, only: [:index] is equivalent to:
+  resources :creatures, only: [:index, :new, :create]
+
+  # resources :creatures, only: [:index, :new, :create] is equivalent to:
   # get "/creatures", to: "creatures#index"
   # get "/creatures/new", to: "creatures#new"
+  # post "/creatures", to: "creatures#create"
 
 
   # The priority is based upon order of creation: first created -> highest priority.
