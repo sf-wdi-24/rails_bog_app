d7e126f41c4bb92159ddac601d8cfef3ebf534dd
stanleyyork <stanleyyork@gmail.com>
2015-12-13 22:22:54 -0800

    edit and update

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -26,4 +26,18 @@ class CreaturesController < ApplicationController
 	    render :show
 	end
 
+	def edit
+		creature_id = params[:id]
+		@creature = Creature.find_by_id(creature_id)
+		render :edit
+	end
+
+	def update
+		creature_id = params[:id]
+		creature = Creature.find_by_id(creature_id)
+		creature_params = params.require(:creature).permit(:name, :description)
+		creature.update_attributes(creature_params)
+		redirect_to creature_path(creature)
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root "creatures#index"
 
-  resources :creatures, only: [:index, :new, :create, :show]
+  resources :creatures, only: [:index, :new, :create, :show, :edit, :update]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
