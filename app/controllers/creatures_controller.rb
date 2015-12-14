75447bb52e04eaf4207be43f7b6809fbe0d2de12
DreTGiant <amassey30@yahoo.com>
2015-12-13 19:02:16 -0800

    routes added

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -7,4 +7,43 @@ class CreaturesController < ApplicationController
     # render the index view (it has access to instance variable)
     render :index
   end
+    # show the new creature form
+  def new
+    render :new
+  end
+   # create a new creature in the database
+  def create
+    # whitelist params and save them to a variable
+    creature_params = params.require(:creature).permit(:name, :description)
+
+    # create a new creature from `creature_params`
+    creature = Creature.new(creature_params)
+
+    # if creature saves, redirect to route that displays
+    # ONLY the newly created creature
+    if creature.save
+      redirect_to creature_path(creature)
+      # redirect_to creature_path(creature) is equivalent to:
+      # redirect_to "/creatures/#{creature.id}"
+    end
+  end
+    def new
+    @creature = Creature.new
+    render :new
+  end
+    # display a specific creature
+  def show
+    # get the creature id from the url params
+    creature_id = params[:id]
+
+    # use `creature_id` to find the creature in the database
+    # and save it to an instance variable
+    @creature = Creature.find_by_id(creature_id)
+
+    # render the show view (it has access to instance variable)
+    render :show
+  end
+
+
+
 end
