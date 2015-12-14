f9b39b7a8da3b4cd3f3cde5cf9a23656dbb5fd90
murcielago17 <murcielago17@hotmail.com>
2015-12-13 23:06:59 -0800

    Define a route to show a specific creature

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -36,4 +36,17 @@ def create
     end
   end
 
+  # display a specific creature
+  def show
+    # get the creature id from the url params
+    creature_id = params[:id]
+
+    # use `creature_id` to find the creature in the database
+    # and save it to an instance variable
+    @creature = Creature.find_by_id(creature_id)
+
+    # render the show view (it has access to instance variable)
+    render :show
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   root "creatures#index"
   # use the resources method to have Rails make an index route for creatures
-  resources :creatures, only: [:index, :new, :create]
+  resources :creatures, only: [:index, :new, :create, :show]
 
-  # resources :creatures, only: [:index, :new, :create] is equivalent to:
+  # resources :creatures, only: [:index, :new, :create, :show] is equivalent to:
   # get "/creatures", to: "creatures#index"
   # get "/creatures/new", to: "creatures#new"
   # post "/creatures", to: "creatures#create"
+  # post "/creatures/:id", to: "creatures#show"
+
 
 
   # The priority is based upon order of creation: first created -> highest priority.
