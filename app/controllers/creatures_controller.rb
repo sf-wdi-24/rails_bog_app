4db99ae561c68e70d4a93957a18582e6cacddbb5
murcielago17 <murcielago17@hotmail.com>
2015-12-13 22:22:36 -0800

    Set up the creatures create action

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -19,4 +19,20 @@ class CreaturesController < ApplicationController
    end
 
 
+# create a new creature in the database
+def create
+  # whitelist params and save them to a variable
+  creature_params = params.require(:creature).permit(:name, :description)
+
+  # create a new creature from `creature_params`
+  creature = Creature.new(creature_params)
+
+  # if creature saves, redirect to route that displays all creatures
+   if creature.save
+     redirect_to creatures_path
+      # redirect_to creatures_path is equivalent to:
+      # redirect_to "/creatures"
+    end
+  end
+
 end
