c73321ed06496bdb94480a022da730dac8bf52f8
murcielago17 <murcielago17@hotmail.com>
2015-12-13 23:27:17 -0800

    Part IV: Delete a creature with destroy (database)   Define a route to destroy a specific creature

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root "creatures#index"
   # use the resources method to have Rails make an index route for creatures
-  resources :creatures, only: [:index, :new, :create, :show, :edit, :update]
+  resources :creatures, only: [:index, :new, :create, :show, :edit, :update, :destroy]
 
-  # resources :creatures, only: [:index, :new, :create, :show, :edit, :update] is equivalent to:
+  # resources :creatures, only: [:index, :new, :create, :show, :edit, :update, :destroy] is equivalent to:
   # get "/creatures", to: "creatures#index"
   # get "/creatures/new", to: "creatures#new"
   # post "/creatures", to: "creatures#create"
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   # get "/creatures/:id/edit", to: "creatures#edit"
   # put "/creatures/:id", to: "creatures#update"
   # patch "/creatures/:id", to: "creatures#update"
+  # delete "/creatures/:id", to: "creatures#destroy"
 
   # resources :creatures, only: [:index, :new, :create, :show] is equivalent to:
   # get "/creatures", to: "creatures#index"
