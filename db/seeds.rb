caa6e18f6083a2eede7a10cce4bb43583a3fce4a
murcielago17 <murcielago17@hotmail.com>
2015-12-13 20:45:04 -0800

    create creature and seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Creature.create({name: "Luke", description: "Jedi"})
+Creature.create({name: "Darth Vader", description: "Father of Luke"})
