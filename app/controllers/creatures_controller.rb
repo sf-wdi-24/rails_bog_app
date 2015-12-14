b53d617e29aa66efe758e8bb90be55f0c5721bec
TheEddyChang <ehchang58@yahoo.com>
2015-12-13 17:13:17 -0800

    new, create, and show

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -3,8 +3,28 @@ class CreaturesController < ApplicationController
 	def index
 		#get all creatures from dbdb and save to instance variable
 		@creatures = Creature.all
+        render :index
+	end
 
-		render :index
+	def new
+		@creature = Creature.new
+		render :new
+	end
+
+	def create
+		creature_params = params.require(:creature).permit(:name, :description)
+		creature = Creature.new(creature_params)
+
+		if creature.save
+			redirect_to creatyre_path(creature)
+		    #equivalent to redirect_to "/creatures/#{creature."
+		end
+	end
+
+	def show
+		creature_id = params[:id]
+		@creature = Creature.find_by_id(creature_id)
+		render :show
 	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root "creatures#index"
 
-  resources :creatures, only: [:index]
+  resources :creatures, only: [:index, :new, :create]
+
+
 end
