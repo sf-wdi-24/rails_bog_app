bc0b9c05dd7ed3a16dfb4c3fcbdca52ed70aa7e2
murcielago17 <murcielago17@hotmail.com>
2015-12-13 23:23:34 -0800

    Define a route to update a specific creature

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
   root "creatures#index"
   # use the resources method to have Rails make an index route for creatures
-  resources :creatures, only: [:index, :new, :create, :show, :edit]
+  resources :creatures, only: [:index, :new, :create, :show, :edit, :update]
 
-  # resources :creatures, only: [:index, :new, :create, :show, :edit] is equivalent to:
+  # resources :creatures, only: [:index, :new, :create, :show, :edit, :update] is equivalent to:
   # get "/creatures", to: "creatures#index"
   # get "/creatures/new", to: "creatures#new"
   # post "/creatures", to: "creatures#create"
-  # post "/creatures/:id", to: "creatures#show"
+  # get "/creatures/:id", to: "creatures#show"
   # get "/creatures/:id/edit", to: "creatures#edit"
+  # put "/creatures/:id", to: "creatures#update"
+  # patch "/creatures/:id", to: "creatures#update"
 
   # resources :creatures, only: [:index, :new, :create, :show] is equivalent to:
   # get "/creatures", to: "creatures#index"
