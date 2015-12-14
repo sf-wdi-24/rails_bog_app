181ba275499a443887aa0cae5a2b6e6820b8d0a8
DreTGiant <amassey30@yahoo.com>
2015-12-13 20:15:39 -0800

    weekend HW

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -43,7 +43,53 @@ class CreaturesController < ApplicationController
     # render the show view (it has access to instance variable)
     render :show
   end
+   # show the edit creature form
+  def edit
+    # get the creature id from the url params
+    creature_id = params[:id]
 
+    # use `creature_id` to find the creature in the database
+    # and save it to an instance variable
+    @creature = Creature.find_by_id(creature_id)
 
+    # render the edit view (it has access to instance variable)
+    render :edit
+  end
+   # update a creature in the database
+  def update
+    # get the creature id from the url params
+    creature_id = params[:id]
 
+    # use `creature_id` to find the creature in the database
+    # and save it to an instance variable
+    creature = Creature.find_by_id(creature_id)
+
+    # whitelist params and save them to a variable
+    creature_params = params.require(:creature).permit(:name, :description)
+
+    # update the creature
+    creature.update_attributes(creature_params)
+
+    # redirect to show page for the updated creature
+    redirect_to creature_path(creature)
+    # redirect_to creature_path(creature) is equivalent to:
+    # redirect_to "/creatures/#{creature.id}"
+  end
+  # delete a creature from the database
+  def destroy
+    # get the creature id from the url params
+    creature_id = params[:id]
+
+    # use `creature_id` to find the creature in the database
+    # and save it to an instance variable
+    creature = Creature.find_by_id(creature_id)
+
+    # destroy the creature
+    creature.destroy
+
+    # redirect to creatures index
+    redirect_to creatures_path
+    # redirect_to creatures_path is equivalent to:
+    # redirect_to "/creatures"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,18 @@
 Rails.application.routes.draw do
   root "creatures#index"
+  resources :creatures
 
   # use the resources method to have Rails make an index route for creatures
-  resources :creatures, only: [:index, :new, :create, :show]
+  resources :creatures, only: [:index, :new, :create, :show, :edit, :destroy]
 
   # resources :creatures, only: [:index] is equivalent to:
   # get "/creatures", to: "creatures#index"
   # get "/creatures/new", to: "creatures#new"
    # post "/creatures", to: "creatures#create"
    # post "/creatures/:id", to: "creatures#show"
-
-
+   # get "/creatures/:id/edit", to: "creatures#edit"
+   # put "/creatures/:id", to: "creatures#update"
+  # patch "/creatures/:id", to: "creatures#update"
+    # delete "/creatures/:id", to: "creatures#destroy"
 
 end
